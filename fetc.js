18acf8878465bb036dcdb9963b955f4bfdcea06a
Siddhant <siddhant11raj@gmail.com>
2024-07-09 15:28:32 +0530

    updated database

--- fetc.js
+++ fetc.js
@@ -7,6 +7,7 @@ async function fun(){
         <td> ${e.id}</td>
         <td> ${e.name}</td>
         <td> ${e.city}</td>
+        <td><button onclick="mydelete(${e.id})">Delete </button> </td>
     </tr>
 
     `).join(" ") 
@@ -15,3 +16,27 @@ async function fun(){
 
 }
 fun()
+
+function mydelete(id){
+    fetch(`http://localhost:3000/student/${id}`,{
+        method:"DELETE"
+    })
+    .then(res=>alert("succesfully deleted"))
+}
+
+function addd(){
+    let data={
+        id:document.getElementById('id').value,
+        name:document.getElementById('name').value,
+        city:document.getElementById('city').value,
+    }
+    fetch("http://localhost:3000/student",{
+        method:"POST",
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+
+
+}
