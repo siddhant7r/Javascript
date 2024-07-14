e8aae9af1ea9582bff33b169ba917fb6663cf30c
Siddhant <siddhant11raj@gmail.com>
2024-07-14 23:18:36 +0530

    made changes in db

--- fetc.js
+++ fetc.js
@@ -8,6 +8,7 @@ async function fun(){
         <td> ${e.name}</td>
         <td> ${e.city}</td>
         <td><button onclick="mydelete(${e.id})">Delete </button> </td>
+        <td><button onclick="myupdate(${e.id})">Edit </button> </td>
     </tr>
 
     `).join(" ") 
@@ -16,6 +17,8 @@ async function fun(){
 
 }
 fun()
+    
+
 
 function mydelete(id){
     fetch(`http://localhost:3000/student/${id}`,{
@@ -33,6 +36,7 @@ function addd(){
     fetch("http://localhost:3000/student",{
         method:"POST",
         headers:{
+
             'content-type':'application/json'
         },
         body:JSON.stringify(data)
@@ -40,3 +44,61 @@ function addd(){
 
 
 }
+
+// var storeid = 0
+// function myupdate(id){
+//     storeid=id;
+//     document.getElementById("myfrm").style.display="block"
+//     document.getElementById("id1").value=id
+// }
+
+// function update(){
+//     let mydata={
+//         id:document.getElementById('id1').value,
+//         name:document.getElementById('name1').value,
+//         city:document.getElementById('city1').value,
+//     }
+//     fetch(`http://localhost:3000/student/${storeid}`,{
+//        method:"PUT",
+//        headers:{
+//         'Content-type':'application/json'
+//        },
+//        body:JSON.stringify(mydata) 
+//     })
+//     .then(res=>alert("Edit Succesfully"))
+//     .catch(res=>alert("Errror"))
+// }
+
+var stid=0
+async function myupdate(id){
+    stid=id
+    let mydata = await fetch(`http://localhost:3000/student/${id}`)
+    let con = await mydata.json()
+
+    let show=document.getElementById('myfrm')
+    let y=`
+    <input type="text" value="${con.id}" id="id1" > <br>
+    <input value="text" value="${con.name}" id="name1"> <br>
+    <input value="text" value="${con.city}" id="city1"> <br>
+    <input value="submit" onclick="edit()" value="update">
+    `
+    show.innerHTML=y
+}
+
+function edit(){
+    let myddd={
+        id:document.getElementById('id1').value,
+        name:document.getElementById('name1').value,
+        city:document.getElementById('city1').value
+    }
+    fetch(`http://localhost:3000/student/${stid}`,{
+        method:"PUT",
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify(myddd)
+    })
+    .then(res=>alert("Edit done"))
+    .catch(res=>alert("Error"))
+}
+
